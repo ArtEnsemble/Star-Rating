7a0bf81fe21bc7dea524157cbdfcf441f7669731
Ryan Day <rmday77@gmail.com>
2020-11-15 21:09:32 -0500

    commit

--- stars.js
+++ stars.js
@@ -15,23 +15,19 @@ function starClick() {
 
 function starSelector(star) {
     
-    //star.classList.add('highlight');
+    
     
     for (i=0; i<starArr.length; i++) {
-        //starArr[i].classList.remove('highlight');
+    
         
-         //console.log('removed!');
+    
         if (starArr[i] === star) {
         console.log(`you clicked star number ${i + 1}`);
-        
-        
-        
-        
+            
         for (j=0; j<=i; j++) {
             starArr[j].classList.add('highlight');
         }
         for (j=starArr.length -1; j>i; j--) {
-            
             starArr[j].classList.remove('highlight');
         }
         return (i);
